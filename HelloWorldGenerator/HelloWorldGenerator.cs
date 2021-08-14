63cd8853c868769501835d407451d01fb2c0ebac
shun-shun123 <shunsuke15vb@gmail.com>
2021-08-15 08:11:45 +0900

    First commit

---
+++ HelloWorldGenerator/HelloWorldGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGeneratorSample
+{
+    [Generator]
+    public class HelloWorldGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sourceBuilder = new StringBuilder(@"
+using System;
+namespace HelloWorldGenerated
+{
+    public static class HelloWorld
+    {
+        public static void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+");
+
+            // using the context, get a list of syntax trees in the users compilation
+            var syntaxTrees = context.Compilation.SyntaxTrees;
+
+            foreach (var tree in syntaxTrees)
+            {
+                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
+            }
+
+            // finish creating the source to inject
+            sourceBuilder.AppendLine(@"
+        }
+    }
+}");
+
+            // inject the created source into the users compilation
+            context.AddSource("helloWorldGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+    }
+}
